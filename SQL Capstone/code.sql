0be74263b6636e3d2c926b2ff3c5db4350d2195f
willa055 <31356899+willa055@users.noreply.github.com>
2019-01-26 14:51:14 -0600

    Codecademy generated from the project submission page.

---
+++ SQL Capstone/code.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM survey
+LIMIT 10;
+
+SELECT question,
+   COUNT(DISTINCT user_id)
+FROM survey
+GROUP BY question;
+
+SELECT *
+FROM quiz
+LIMIT 5;
+
+SELECT *
+FROM home_try_on
+LIMIT 5;
+
+SELECT *
+FROM purchase
+LIMIT 5;
+
+SELECT DISTINCT q.user_id,
+   h.user_id IS NOT NULL AS 'is_home_try_on',
+   h.number_of_pairs,
+   p.user_id IS NOT NULL AS 'is_purchase'
+FROM quiz q
+LEFT JOIN home_try_on h
+   ON q.user_id = h.user_id
+LEFT JOIN purchase p
+   ON p.user_id = q.user_id
+LIMIT 10;
